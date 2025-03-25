123d0c4067141c1f836cc7e1142ae370bbc42624
spoxle <colemaron@icloud.com>
2025-03-24 17:29:32 -0700

    made font previewer better

--- Font Previewer/app.js
+++ Font Previewer/app.js
@@ -3,76 +3,91 @@
 const key = "AIzaSyDkjOgHdwmrIIsNXTeVC1ws8RjQloTHJE4";
 const api = `https://www.googleapis.com/webfonts/v1/webfonts?key=${key}`;
 
-// fetch json
+// fetch fonts
 
-const fetchJSON = async url => fetch(url).then(response => response.json());
+async function fetchFonts(sort = "trending") {
+	const url = `${api}&sort=${sort}`;
+	const response = await fetch(url).then(response => response.json());
 
-// fetch google fonts
-
-const fetchFonts = async sort => await fetchJSON(`${api}&sort=${sort}`);
-
-// search fonts
-
-const searchForm = document.getElementById("search-form");
-const fontResults = document.getElementById("results");
-
-const preview = document.getElementById("preview");
-
-async function searchFonts() {
-	const data = new FormData(searchForm);
-
-	const query = data.get("query").toLowerCase();
-	const sort = data.get("sort");
-
-	fontResults.textContent = "Loading...";
-
-	const fonts = await fetchFonts(sort).then(data => data.items);
-	const results = fonts.filter(font => font.family.toLowerCase().includes(query));
-
-	while (fontResults.firstChild) {
-		fontResults.removeChild(fontResults.firstChild);
-	}
-
-	if (results.length > 0) {
-		for (const font of results) {
-			const div = document.createElement("div");
-			div.classList.add("font");
-	
-			// add family name
-	
-			const family = document.createElement("h4");
-			family.classList.add("family");
-			family.textContent = font.family;
-	
-			div.appendChild(family);
-	
-			// add example text
-	
-			const sample = document.createElement("p");
-			sample.classList.add("sample");
-
-			sample.style.fontFamily = font.files.regular;
-			sample.textContent = preview.value;
-	
-			div.appendChild(sample);
-	
-			// add font
-	
-			fontResults.appendChild(div);
-		}
-	} else {
-		fontResults.textContent = `No results for ${query}`;
-	}
+	return response.items;
 }
 
-// update results
+// load font
 
-searchForm.addEventListener("submit", event => event.preventDefault());
-searchForm.addEventListener("input", searchFonts);
+async function loadFont(font) {
+	const url = `https://fonts.googleapis.com/css?family=${font}`;
 
-searchFonts();
+	const link = document.createElement("link");
+	link.rel = "stylesheet";
+	link.href = url;
 
-// appl preview
+	document.head.appendChild(link);
+}
+
+// scroll observer
+
+const observer = new IntersectionObserver((entries, observer) => {
+	for (const entry of entries) {
+		if (entry.isIntersecting) {
+			const element = entry.target;
+			const font = element.dataset.font;
+
+			loadFont(font.replace(/\s+/g, "+"));
+
+			observer.unobserve(element);
+		}
+	}
+}, { root: results, threshold: 0 } );
+
+// create font element
+
+function createFontElement(font) {
+	// main font element
+
+	const div = document.createElement("div");
+	div.dataset.font = font.family;
+	div.classList.add("font")
+
+	// font family name
+
+	const h4 = document.createElement("h4");
+	h4.textContent = font.family;
+
+	// sample text
+	
+	const sample = document.createElement("p");
+	sample.style.fontFamily = font.family;
+	sample.textContent = preview.value;
+	sample.classList.add("sample");
+
+	// download
+
+	const a = document.createElement("a");
+	a.textContent = "Download";
+	a.href = font.files.regular;
+	a.classList.add("download");
+
+	// append children
+	
+	div.appendChild(h4);
+	div.appendChild(sample);
+	div.appendChild(a);
+
+	return div;
+}
+
+// create elements
+
+fetchFonts().then(fonts => {
+	for (const font of fonts) {
+		const element = createFontElement(font);
+
+		results.appendChild(element);
+		observer.observe(element);
+	}
+});
+
+// update samples
 
 preview.addEventListener("input", () => {
 	const samples = document.querySelectorAll(".sample");
@@ -81,3 +96,35 @@ preview.addEventListener("input", () => {
 		sample.textContent = preview.value;
 	}
 })
+
+// search fonts
+
+const form = document.getElementById("search-form");
+
+form.addEventListener("input", event => {
+	event.preventDefault();
+
+	// get form values
+
+	const data = new FormData(form);
+	const query = data.get("query").toLowerCase();
+
+	const sort = data.get("sort");
+
+	// clear results
+
+	results.innerHTML = "";
+	
+	// filter results
+
+	fetchFonts(sort).then(fonts => {
+		const filtered = fonts.filter(font => font.family.toLowerCase().includes(query));
+
+		for (const font of filtered) {
+			const element = createFontElement(font);
+
+			results.appendChild(element);
+			observer.observe(element);
+		}
+	})
+});
